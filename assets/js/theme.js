fc161b0cd3489762cfdc711b91e7bba67da71fc6
kaiou-web <stuytown6@gmail.com>
2024-09-14 14:23:43 -0400

    Update theme.js

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,12 +1,14 @@
 // Has to be in the head tag, otherwise a flicker effect will occur.
 
-// Toggle through light and dark theme settings only
+// Toggle through light, dark, and system theme settings.
 let toggleThemeSetting = () => {
   let themeSetting = determineThemeSetting();
-  if (themeSetting == "light") {
+  if (themeSetting == "system") {
+    setThemeSetting("light");
+  } else if (themeSetting == "light") {
     setThemeSetting("dark");
   } else {
-    setThemeSetting("light");
+    setThemeSetting("system");
   }
 };
 
@@ -213,13 +215,20 @@ let determineThemeSetting = () => {
   return themeSetting;
 };
 
-// Determine the expected state of the theme toggle, which can be "dark" or "light". Default is "light".
-let determineThemeSetting = () => {
-  let themeSetting = localStorage.getItem("theme");
-  if (themeSetting != "dark" && themeSetting != "light") {
-    themeSetting = "dark"; // Set "dark" as the default theme
+// Determine the computed theme, which can be "dark" or "light". If the theme setting is
+// "system", the computed theme is determined based on the user's system preference.
+let determineComputedTheme = () => {
+  let themeSetting = determineThemeSetting();
+  if (themeSetting == "system") {
+    const userPref = window.matchMedia;
+    if (userPref && userPref("(prefers-color-scheme: dark)").matches) {
+      return "dark";
+    } else {
+      return "light";
+    }
+  } else {
+    return themeSetting;
   }
-  return themeSetting;
 };
 
 let initTheme = () => {
